b1443db486663014f69e93dfda21b74a5431e7aa
Mohammed Akhnas Gawai <akhnasg@gmail.com>
2023-05-04 11:43:55 +0530

    added footer and background image for index page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+console.log("ENTERED");
+
 const getElement = (selector) => {
   const element = document.querySelector(selector)
 
@@ -13,12 +15,3 @@ const navBtnDOM = getElement('.nav-btn')
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
-
-var counter = 1;
-setInterval(function() {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 3){
-        counter = 1;
-    }
-}, 4000);
